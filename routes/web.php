d357c8307af9d070f04cc790e8e00933caa0a876
fuadadhim24 <fuadadhim24@gmail.com>
2024-07-14 22:23:30 +0700

    init: index kelas and resep page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {return view('admin.dashboard');})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/kelas', function () {return view('admin.classes.index');})->middleware(['auth', 'verified'])->name('kelas');
+Route::get('/resep', function () {return view('admin.recipes.index');})->middleware(['auth', 'verified'])->name('resep');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
